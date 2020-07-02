d09ae3e0e5bcb7e7ff25023e1c6131a9534adcc1
StepWhite <916529626@qq.com>
2020-07-02 15:49:58 +0800

    accomplish requirement and change package name

--- src/adapters/base64uploadadapter.js
+++ src/adapters/base64uploadadapter.js
@@ -78,9 +78,14 @@ class Adapter {
 		return new Promise( ( resolve, reject ) => {
 			const reader = this.reader = new window.FileReader();
 
+			// ##Less-Adjust
 			reader.addEventListener( 'load', () => {
-				resolve( { default: reader.result } );
+				// resolve( { default: reader.result } );
+				this.compressBase64Img(reader.result, (b)=>{
+					resolve( { default: b } );
+				})
 			} );
+			// ##Less-Adjust
 
 			reader.addEventListener( 'error', err => {
 				reject( err );
@@ -105,4 +110,57 @@ class Adapter {
 	abort() {
 		this.reader.abort();
 	}
+
+	// ##Less-Adjust
+	// 图片处理
+	/**
+	 * 
+	 * @param {图片base64字符串} base64 
+	 * @param {*结果回调} callback 
+	 * @param {*压缩系数0-1之间,默认0.6} quality 
+	 * @param {*宽高限制，如果宽或高超过则等比例缩小至该值，默认2048} widthHeightMax 
+	 * @param {*图片最大容量限制，kb值，默认1024} sizeMax 
+	 */
+	compressBase64Img (base64, callback, quality=0.6, widthHeightMax=2048, sizeMax=1024) {
+		var newImage = new Image();
+		// var quality = 0.6;    //压缩系数0-1之间
+		// console.log("quality/widthHeightMax/sizeMax = ", quality,'/', widthHeightMax,'/', sizeMax);
+		newImage.src = base64;
+		newImage.setAttribute("crossOrigin", 'Anonymous');	//url为外域时需要
+		var imgWidth, imgHeight;
+		newImage.onload = function () {
+			imgWidth = this.width;
+			imgHeight = this.height;
+			var canvas = document.createElement("canvas");
+			var ctx = canvas.getContext("2d");
+			if (Math.max(imgWidth, imgHeight) > widthHeightMax) {
+				if (imgWidth > imgHeight) {
+					canvas.width = widthHeightMax;
+					canvas.height = widthHeightMax * imgHeight / imgWidth;
+				} else {
+					canvas.height = widthHeightMax;
+					canvas.width = widthHeightMax * imgWidth / imgHeight;
+				}
+			} else {
+				canvas.width = imgWidth;
+				canvas.height = imgHeight;
+				quality = 0.6;
+			}
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			ctx.drawImage(this, 0, 0, canvas.width, canvas.height);
+			var base64 = canvas.toDataURL("image/jpeg", quality); //压缩语句
+			// 如想确保图片压缩到自己想要的尺寸,如要求在sizeMax kb之间，请加以下语句，quality初始值根据情况自定
+			while (base64.length / 1024 > sizeMax) {
+				quality -= 0.01;
+				base64 = canvas.toDataURL("image/jpeg", quality);
+			}
+			// 防止最后一次压缩低于最低尺寸，只要quality递减合理，无需考虑
+			// while (base64.length / 1024 < 50) {
+			// 	quality += 0.001;
+			// 	base64 = canvas.toDataURL("image/jpeg", quality);
+			// }
+			callback(base64);//必须通过回调函数返回，否则无法及时拿到该值
+		}
+	}
+	// ##Less-Adjust
 }
